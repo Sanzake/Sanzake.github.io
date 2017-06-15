f337a2d953497d14894b2bbdabfc111038b72129
Sanzake <danhab20@gmail.com>
2017-06-15 19:00:18 +0300

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,7 @@
+var cards = [];
+	function compareRandom(a, b) {
+  return Math.random() - 0.5;
+}
+function newdeck() {
+	cards.push("Первая", "Вторая", "Третья", "Четвертая", "Пятая", "Шестая", "Седьмая", "Восьмая", "Девятая");
+}
